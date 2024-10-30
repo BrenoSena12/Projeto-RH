2f8b83461c49af14fec06be8f40c11cd3b368473
Breno de Sena Rocha <brenodesenarocha@gmail.com>
2024-10-30 16:35:41 -0300

    edicao e salvamento de dados
    finalizei a parte de edição de registros e salvamento no banco de dados

---
+++ edicao/salva_edit.php
@@ -0,0 +1,24 @@
+<?php 
+
+    include_once('../config.php');
+
+    if(isset($_POST['update']))
+    {
+        $id              = $_POST['id'];
+        $cpf             = $_POST['cpf'];
+        $nome            = $_POST['nome'];
+        $telefone        = $_POST['telefone'];
+        $endereco        = $_POST['endereco'];
+        $data_nascimento = $_POST['data_nascimento'];
+        $cargo           = $_POST['cargo'];
+        $data_admissao   = $_POST['data_admissao'];
+        $salario         = $_POST['salario'];
+
+        $sqlUpdate = "UPDATE tb_cadastro_funcionarios SET cpf='$cpf', nome='$nome', telefone='$telefone', endereco='$endereco', data_nascimento='$data_nascimento', cargo='$cargo', data_admissao='$data_admissao', salario='$salario' WHERE id='$id'";
+        
+        $result = $conexao -> query($sqlUpdate);
+    }
+
+    header('Location: ../pesquisa/pesquisa.php');
+
+?>
